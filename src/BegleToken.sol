0c84a40955dfe3d97847350674653a91a5105249
Y0sefTamer <programmeryosef@gmail.com>
2026-03-26 11:57:25 +0200

    add functionalty for oyr ERC20 token

--- src/BegleToken.sol
+++ src/BegleToken.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract BegleToken {}
+import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+
+contract BegleToken is ERC20, Ownable {
+    constructor() ERC20("Begle Token", "BGL") Ownable(msg.sender) {
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
