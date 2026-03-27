23283d800e9eff7b50ea7ab15b9516a332c10ba2
Y0sefTamer <programmeryosef@gmail.com>
2026-03-27 06:29:30 +0200

    Start adding the merkle logic

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract MerkleAirdrop {}
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+contract MerkleAirdrop {
+    address[] claimers;
+    bytes32 immutable i_merkleRoot;
+    IERC20 immutable i_airdropToken;
+
+    cunstructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken
+    }  
+
+}
