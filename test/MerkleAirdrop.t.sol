f689d4f50e32511c745a828e88f282c320552b83
Y0sefTamer <programmeryosef@gmail.com>
2026-04-02 11:24:31 +0200

    modfing the tests

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -1,45 +1,55 @@
-// SPDX-LICENSE-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
-import {Test, console} from "forge-std/Test.sol";
-import {BegleToken} from "../src/BegleToken.sol";
-import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
-import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
+import {MerkleAirdrop} from "../../src/MerkleAirdrop.sol";
+import {BegleToken} from "../../src/BegleToken.sol";
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+import {DeployMerkleAirdrop} from "../../script/DeployMerkleAirdrop.s.sol";
 
 contract MerkleAirdropTest is Test {
-    BegleToken private token;
-    MerkleAirdrop private airdrop;
+    MerkleAirdrop airdrop;
+    BegleToken token;
+    address gasPayer;
+    address user;
+    uint256 userPrivKey;
 
-    bytes32 private root = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
-    uint256 private constant AMOUNT = 25 * 1e18;
-    bytes32 private proof1 = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
-    bytes32 private proof2 = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
-    bytes32[] private PROOF = [proof1, proof2];
-    address private user;
-    uint256 private userPrivateKey;
+    bytes32 merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 amountToCollect = (25 * 1e18); // 25.000000
+    uint256 amountToSend = amountToCollect * 4;
+
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] proof = [proofOne, proofTwo];
 
     function setUp() public {
         token = new BegleToken();
-        // mint some tokens to the airdrop contract
-        token.mint(token.owner(), 1000 * 10 ** 18);
-        airdrop = new MerkleAirdrop(root, token);
-        (user, userPrivateKey) = makeAddrAndKey("user");
-        // transfer some tokens to the airdrop contract
-        token.transfer(address(airdrop), 1000 * 10 ** 18);
+        airdrop = new MerkleAirdrop(merkleRoot, token);
+        token.mint(token.owner(), amountToSend);
+        token.transfer(address(airdrop), amountToSend);
+
+        gasPayer = makeAddr("gasPayer");
+        (user, userPrivKey) = makeAddrAndKey("user");
     }
 
-    function testClaim() public {
-        // claim the airdrop for the first account
-        // airdrop.claim(address(0x123), 100 * 10 ** 18, new bytes32[](2));
-        // check that the balance of the first account is correct
-        //assertEq(token.balanceOf(address(0x123)), 100 * 10 ** 18);
+    function signMessage(uint256 privKey, address account) public view returns (uint8 v, bytes32 r, bytes32 s) {
+        bytes32 hashedMessage = airdrop.getMessageHash(account, amountToCollect);
+        (v, r, s) = vm.sign(privKey, hashedMessage);
+    }
+
+    function testUsersCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
 
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT, PROOF);
+        // get the signature
+        vm.startPrank(user);
+        (uint8 v, bytes32 r, bytes32 s) = signMessage(userPrivKey, user);
+        vm.stopPrank();
 
+        // gasPayer claims the airdrop for the user
+        vm.prank(gasPayer);
+        airdrop.claim(user, amountToCollect, proof, v, r, s);
         uint256 endingBalance = token.balanceOf(user);
-        console.log("ending balance: ", endingBalance);
-        assertEq(endingBalance - startingBalance, AMOUNT);
+        console.log("Ending balance: %d", endingBalance);
+        assertEq(endingBalance - startingBalance, amountToCollect);
     }
 }
